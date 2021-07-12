e8880d7290597af7f6568e223acbfd1f05760d10
Rafael Santos <rafael_16001@hotmail.com>
2021-07-12 09:54:01 -0300

    Interface implementada

---
+++ src/main/java/model/entities/Vehicle.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.entities;
+
+/**
+ *
+ * @author rafae
+ */
+public class Vehicle{
+    private String model;
+    
+    public Vehicle(){
+        
+    }
+    
+    public Vehicle(String model){
+        this.model = model;
+        
+    }
+    
+}
